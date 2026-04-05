a6403a7259120908c66ef4b156ffc60a528c3316
Latae-Davis <latae.davis@gmail.com>
2026-04-05 18:44:25 -0400

    Update 01_data_cleaning.sql

--- sql_queries/01_data_cleaning.sql
+++ sql_queries/01_data_cleaning.sql
@@ -1,1 +1,17 @@
+-- Load source tables
+CREATE TABLE orders AS
+SELECT * FROM read_csv_auto('olist_orders_dataset.csv');
 
+CREATE TABLE order_items AS
+SELECT *
+FROM read_csv(
+  'olist_order_items_dataset.csv',
+  delim = ',',
+  header = true
+);
+
+CREATE TABLE products AS
+SELECT * FROM read_csv_auto('olist_products_dataset.csv');
+
+CREATE TABLE customers AS
+SELECT * FROM read_csv_auto('olist_customers_dataset.csv');
