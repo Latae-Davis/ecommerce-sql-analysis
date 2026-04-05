665b98c62095e92f8a96345b420df802abef3023
Latae-Davis <latae.davis@gmail.com>
2026-04-05 18:53:26 -0400

    Update 01_data_cleaning.sql

--- sql_queries/01_data_cleaning.sql
+++ sql_queries/01_data_cleaning.sql
@@ -15,3 +15,18 @@ SELECT * FROM read_csv_auto('olist_products_dataset.csv');
 
 CREATE TABLE customers AS
 SELECT * FROM read_csv_auto('olist_customers_dataset.csv');
+
+-- Check for missing product categories
+SELECT COUNT(*) AS missing_category
+FROM products
+WHERE product_category_name IS NULL;
+
+-- Replace missing categories with 'Unknown'
+UPDATE products
+SET product_category_name = 'Unknown'
+WHERE product_category_name IS NULL;
+
+-- Confirm no missing categories remain
+SELECT COUNT(*) AS remaining_missing_category
+FROM products
+WHERE product_category_name IS NULL;
